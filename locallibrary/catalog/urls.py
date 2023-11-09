ed9c2f439624d34d4c1b8393e93b6767ab4befcf
79234 <marusya0323@gmail.com>
2023-11-10 00:00:29 +0700

    URL-преобразования

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
